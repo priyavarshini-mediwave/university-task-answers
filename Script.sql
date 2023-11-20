37657729646b89e9af9c71a217f6f35cc45ad22c
priyavarshini-mediwave <priyavarshini@mindwaveventures.com>
2023-11-20 19:02:27 +0530

    script

--- Script.sql
+++ Script.sql
@@ -122,3 +122,72 @@ group by s.std_name ,s.std_id ;
 --update marks set marks=99 where mark_id=1;
 --insert into students (std_name,dob,phone_no,address,joining_batch,course_id,college_id)values('Radha','2002-11-22',9876543211,'Tirunelveli',2020,1,2)
 --insert into marks (std_id,sem_id,marks,sub_id) values (12,1,55,5),(12,1,35,6)
+
+
+-- Task 3
+
+--1,
+
+update marks set "marks"=40 where marks between 35 and 39
+--3,
+--SELECT
+--    select_list
+--INTO [ TEMPORARY | TEMP | UNLOGGED ] [ TABLE ] new_table_name
+--FROM
+--    table_name
+--WHERE
+--    search_condition;
+
+
+--taking query 9 from task 2
+select s.std_id,s.std_name into temporary table temp_table from students s LEFT JOIN marks m ON s.std_id  = m.std_id 
+WHERE m.marks = -1 OR m.marks IS NULL
+group by s.std_name ,s.std_id ;
+
+select * from temp_table;
+--6,
+
+insert into marks (std_id,sem_id,marks,sub_id) values (11,1,98,21),(11,1,99,22),(11,1,55,23),(11,1,100,24)
+insert into marks (std_id,sem_id,marks,sub_id) values (12,1,56,5),(12,1,35,6)
+select * from marks 
+--DELETE  FROM
+--    basket a
+--        USING basket b
+--WHERE
+--    a.id > b.id
+--    AND a.fruit = b.fruit;
+--delete from marks m1 using marks m2 where m1.mark_id > m2.mark_id and m1.marks = m2.marks
+--select * from marks 
+insert into marks (mark_id ,std_id,sem_id,marks,sub_id) values (27,11,1,98,21),(28,11,1,99,22),(29,11,1,97,23),(30,11,1,100,24)
+--delete from marks where mark_id =33
+
+--DELETE FROM table_name
+--WHERE id IN
+--    (SELECT id
+--    FROM 
+--        (SELECT id,
+--         ROW_NUMBER() OVER( PARTITION BY column_1,
+--         column_2
+--        ORDER BY  id ) AS row_num
+--        FROM table_name ) t
+--        WHERE t.row_num > 1 );
+begin transaction ;
+
+delete from marks 
+where mark_id in (select mark_id 
+from 
+(select mark_id, 
+row_number() over (partition by std_id,sub_id 
+order by mark_id)as row_num 
+from marks )t
+WHERE t.row_num > 1 );
+
+select  * from marks m 
+commit
+
+update marks set "marks" = 35 where mark_id = 26 returning *;
+
+begin transaction ;
+	update marks set "marks" =98  where "marks" = 99 returning *;
+
+rollback;
